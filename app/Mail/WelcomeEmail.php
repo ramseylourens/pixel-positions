dfb637b0db391110948d744db8bc311d3cd598f0
Ramsey <rcrlourens@gmail.com>
2026-02-18 23:27:44 +0100

    Patch

--- app/Mail/WelcomeEmail.php
+++ app/Mail/WelcomeEmail.php
@@ -5,6 +5,7 @@ namespace App\Mail;
 use App\Models\User;
 use Illuminate\Bus\Queueable;
 use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Address;
 use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
@@ -20,7 +21,11 @@ class WelcomeEmail extends Mailable
     public function envelope(): Envelope
     {
         return new Envelope(
+            from: new Address('no-reply@ramseylourens.com', 'Pixel Positions'),
             subject: 'Welcome to ' . config('app.name'),
+            replyTo: [
+                new Address('pixel-positions@ramseylourens.com', 'Pixel Positions Support Team'),
+            ],
         );
     }
 
