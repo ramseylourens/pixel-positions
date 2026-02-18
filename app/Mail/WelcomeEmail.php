27e6319a3db6ced236b67a566e14765b55dd85b3
Ramsey <rcrlourens@gmail.com>
2026-02-18 22:45:18 +0100

    Patch

--- app/Mail/WelcomeEmail.php
+++ app/Mail/WelcomeEmail.php
@@ -4,7 +4,6 @@ namespace App\Mail;
 
 use App\Models\User;
 use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
@@ -14,40 +13,24 @@ class WelcomeEmail extends Mailable
 {
     use Queueable, SerializesModels;
 
-    /**
-     * Create a new message instance.
-     */
-    public $user;  // Must be public!
+    public function __construct(
+        public User $user
+    ) {}
 
-    public function __construct(User $user)
-    {
-        $this->user = $user;
-    }
-
-    /**
-     * Get the message envelope.
-     */
     public function envelope(): Envelope
     {
-        return $this->subject('Welcome to ' . config('app.name'))
-                    ->markdown('emails.welcome');
-    }
-
-    /**
-     * Get the message content definition.
-     */
-    public function content(): Content
-    {
-        return new Content(
-            view: 'view.name',
+        return new Envelope(
+            subject: 'Welcome to ' . config('app.name'),
         );
     }
 
-    /**
-     * Get the attachments for the message.
-     *
-     * @return array<int, \Illuminate\Mail\Mailables\Attachment>
-     */
+    public function content(): Content
+    {
+        return new Content(
+            markdown: 'emails.welcome',
+        );
+    }
+
     public function attachments(): array
     {
         return [];
